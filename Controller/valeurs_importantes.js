4caae46ebf1d34619c960c6a4b40ab218a264c26
TuilierGit <thomas.tuilier.git@gmail.com>
2023-12-04 20:52:29 +0100

    Mise en place des head-box avec des valeurs arbitraires

---
+++ Controller/valeurs_importantes.js
@@ -0,0 +1,23 @@
+
+
+last_net_worth = 1500;
+income = 450;
+expenses = 125;
+savingsRate = 4300;
+
+function get_my_value(val){
+    switch (val){ 
+    case "patrimoine_net":
+        return last_net_worth;
+    case "revenus":
+        return income;
+    case "depenses":
+        return expenses
+    case "taux_epargne":
+        return savingsRate;
+    default :
+        console.log('ERROR');
+    }
+
+    
+}
